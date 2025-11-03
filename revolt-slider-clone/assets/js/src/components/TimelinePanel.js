3e4daca499a699a845476a2de442366db8566306
ranjithdhanarajan-cmd <ranjith.dhanarajan@gmail.com>
2025-11-03 17:59:31 +0530

    Create TimelinePanel.js

---
+++ revolt-slider-clone/assets/js/src/components/TimelinePanel.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const TimelinePanel = () => {
+    return (
+        <section className="revolt-timeline">
+            <header>
+                <h2>Timeline</h2>
+            </header>
+            <div className="revolt-timeline__tracks">
+                <p>Layer animation tracks will appear here.</p>
+            </div>
+        </section>
+    );
+};
+
+export default TimelinePanel;
