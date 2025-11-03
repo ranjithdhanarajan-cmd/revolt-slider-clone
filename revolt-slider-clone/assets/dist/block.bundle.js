b7ca05cbf2dd6622510c9f1ef6bd6f13987d84de
ranjithdhanarajan-cmd <ranjith.dhanarajan@gmail.com>
2025-11-03 19:49:12 +0530

    Update block.bundle.js

--- revolt-slider-clone/assets/dist/block.bundle.js
+++ revolt-slider-clone/assets/dist/block.bundle.js
@@ -44,3 +44,65 @@
         },
     });
 })();
+revolt-slider-clone/assets/dist/frontend.bundle.js
+New
++57
+-0
+
+(function () {
+    const modules = document.querySelectorAll('.revolt-module');
+
+    const playLayerAnimation = function (layer, animation) {
+        if (!animation) {
+            return;
+        }
+
+        const to = animation.to || {};
+        const duration = animation.duration || 600;
+        const easing = animation.easing || 'ease-out';
+
+        layer.style.transition = 'all ' + duration + 'ms ' + easing;
+        if (to.opacity !== undefined) {
+            layer.style.opacity = to.opacity;
+        }
+        if (to.y !== undefined) {
+            layer.style.transform = 'translateY(' + to.y + 'px)';
+        }
+    };
+
+    const prepareLayer = function (layer, animation) {
+        if (!animation || !animation.from) {
+            return;
+        }
+
+        const from = animation.from;
+        if (from.opacity !== undefined) {
+            layer.style.opacity = from.opacity;
+        }
+        if (from.y !== undefined) {
+            layer.style.transform = 'translateY(' + from.y + 'px)';
+        }
+    };
+
+    modules.forEach(function (module) {
+        module.querySelectorAll('.revolt-layer').forEach(function (layer) {
+            const data = layer.getAttribute('data-animations');
+            if (!data) {
+                return;
+            }
+
+            try {
+                const animations = JSON.parse(data);
+                if (animations.length) {
+                    prepareLayer(layer, animations[0]);
+                    setTimeout(function () {
+                        playLayerAnimation(layer, animations[0]);
+                    }, 100);
+                }
+            } catch (error) {
+                // eslint-disable-next-line no-console
+                console.warn('Revolt Slider animation parse error', error);
+            }
+        });
+    });
+})();
